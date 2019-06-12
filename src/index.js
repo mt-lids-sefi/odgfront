6bae73e68723a29afd1ef2e51649d3310a88e069
mar <tejedamarcia@gmail.com>
2019-06-12 00:53:00 -0300

    csv reader

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import CSVReader from "react-csv-reader";
+
+const handleForce = data => {
+  console.log(data);
+};
+
+const reader = (
+  <div className="container">
+    <CSVReader
+      cssClass="react-csv-input"
+      label="Select CSV with secret Death Star statistics"
+      onFileLoaded={handleForce}
+    />
+    <p>and then open the console</p>
+  </div>
+);
 
 ReactDOM.render(
-  <App />,
+  //<App />,
+  reader,
   document.getElementById('root')
 );
