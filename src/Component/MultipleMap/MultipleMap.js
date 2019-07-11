f2486a9969c0335cd44498ba7fd3c8fa8ce4fe71
marcia-t <tejedamarcia@gmail.com>
2019-07-11 15:52:22 -0300

    color markers

--- src/Component/MultipleMap/MultipleMap.js
+++ src/Component/MultipleMap/MultipleMap.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import Cylon from "../LoadingComponents/Cylon"
 import { Map, TileLayer, Marker } from "react-leaflet";
 import MarkerClusterGroup from 'react-leaflet-markercluster';
-
+import * as L from 'leaflet'
 
 class MutipleMap extends Component {
     constructor(props) {
@@ -28,14 +28,14 @@ class MutipleMap extends Component {
         if(status===200)
         {
             const data = promise.data;
-            this.setState({dataMap1:data})
+            this.setState({dataMapA:data})
         }
         promise = await axios.get("http://localhost:8000/map/"+doc_ids[1])
         status = promise.status;
         if(status===200)
         {
             const data = promise.data;
-            this.setState({dataMap2:data})
+            this.setState({dataMapB:data})
         }
         
         
@@ -44,29 +44,47 @@ class MutipleMap extends Component {
     }
 
   
+    
 
-
-    makeMarkers(dataMap){
+    makeMarkers(dataMap, icon){
       let markers = []
       for (let element in Object.keys(dataMap.coords) ) {
         let lat = dataMap.coords[element][dataMap.lat_col]
         let lon = dataMap.coords[element][dataMap.lon_col]
-        markers.push(<Marker  key={element} position={[lat, lon]}/>) 
+        markers.push(<Marker  key={element} position={[lat, lon]} icon={icon} />) 
       }
       return markers
     }
 
   render() {
+    const greenIcon = new L.Icon({
+      iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
     
-    if (this.state.dataMap1 == null || this.state.dataMap2 == null) {
+    const violetIcon = new L.Icon({
+      iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+
+    if (this.state.dataMapA == null || this.state.dataMapB == null) {
       return <Cylon/>
     }
     else{
       const center = [-34.6131500, -58.3772300]
       const zoom = 13
-     
-      let markers1 = this.makeMarkers(this.state.dataMap1)
-      let markers2 = this.makeMarkers(this.state.dataMap2)
+      
+      let markersA = this.makeMarkers(this.state.dataMapA, greenIcon)
+      let markersB = this.makeMarkers(this.state.dataMapB, violetIcon)
       return (
         <div>
           <Map className="markercluster-map" center={center} zoom={zoom} maxZoom={18}>
@@ -75,8 +93,8 @@ class MutipleMap extends Component {
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
             <MarkerClusterGroup>
-              {markers1}
-              {markers2}
+              {markersA}
+              {markersB}
           </MarkerClusterGroup>
           </Map>
         </div>
