43b8c2296c161a3baad52c8ba672d7a377a9ab32
mar <tejedamarcia@gmail.com>
2019-07-24 00:58:58 -0300

    showing data

--- src/Component/DataTable/DataTable.js
+++ src/Component/DataTable/DataTable.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
 import TableCell from '@material-ui/core/TableCell';
 import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
@@ -21,6 +22,8 @@ const useStyles1 = makeStyles(theme => ({
     marginLeft: theme.spacing(2.5),
   },
 }));
+
+
 
 function TablePaginationActions(props) {
   const classes = useStyles1();
@@ -80,25 +83,8 @@ TablePaginationActions.propTypes = {
   rowsPerPage: PropTypes.number.isRequired,
 };
 
-function createData(name, calories, fat) {
-  return { name, calories, fat };
-}
 
-const rows = [
-  createData('Cupcake', 305, 3.7),
-  createData('Donut', 452, 25.0),
-  createData('Eclair', 262, 16.0),
-  createData('Frozen yoghurt', 159, 6.0),
-  createData('Gingerbread', 356, 16.0),
-  createData('Honeycomb', 408, 3.2),
-  createData('Ice cream sandwich', 237, 9.0),
-  createData('Jelly Bean', 375, 0.0),
-  createData('KitKat', 518, 26.0),
-  createData('Lollipop', 392, 0.2),
-  createData('Marshmallow', 318, 0),
-  createData('Nougat', 360, 19.0),
-  createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
 
 const useStyles2 = makeStyles(theme => ({
   root: {
@@ -115,12 +101,29 @@ const useStyles2 = makeStyles(theme => ({
 
 export default function DataTable(props) {
   const classes = useStyles2();
+
+  const header = props.header
+  const data = props.data
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  let rows = []
+  for (const key in data) {
+    rows.push(data[key])
+  }
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-  /*const data = props.data
-  const header = props.header
-  const rows = data.rows*/
+
+
+  let heads = []
+  let keyHead=0
+  header.map(h => {
+    keyHead+=1
+    heads.push(<TableCell key={keyHead}>{h}</TableCell>)
+  })  
+  
+
+  
+  
 
   function handleChangePage(event, newPage) {
     setPage(newPage);
@@ -131,18 +134,18 @@ export default function DataTable(props) {
     setPage(0);
   }
 
+  
   return (
     <Paper className={classes.root}>
       <div className={classes.tableWrapper}>
         <Table className={classes.table}>
+          <TableHead><TableRow>{heads}</TableRow></TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
-              <TableRow key={row.name}>
-
-                <TableCell component="th" scope="row">{row.name}</TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-
+          {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+              <TableRow key={row.id}>
+                {header.map(key => (
+                  <TableCell component="th" scope="row" key={key}>{row[key]} </TableCell>
+                ))}
               </TableRow>
             ))}
 
