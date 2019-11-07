610fc1aaee39298b0663bd1148107668ca91116b
marcia-t <tejedamarcia@gmail.com>
2019-11-07 15:34:38 -0300

    layout

--- src/Component/Clusterize/Clusterizer.js
+++ src/Component/Clusterize/Clusterizer.js
@@ -4,17 +4,18 @@ import PropTypes from 'prop-types';
 import Cylon from '../LoadingComponents/Cylon';
 import SimpleMap from '../Maps/SimpleMap'
 import axios from "axios";
+import Grid from '@material-ui/core/Grid';
+import DataTable from '../DataTable/DataTable'
+import { Paper } from '@material-ui/core';
 
 const styles = theme => ({
     root: {
-      flexGrow: 1,
-      overflow: 'hidden',
-      padding: theme.spacing(0, 3),
+      flexGrow: 1
     },
-    paper: {
-      maxWidth: 400,
-      margin: `${theme.spacing(1)}px auto`,
+    paper: { 
       padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
     },
     button: {
       margin: theme.spacing(1),
@@ -29,7 +30,8 @@ class Clusterizer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     file_id : this.props.location.mapProps.file_id,
+     //file_id : this.props.location.mapProps.file_id,
+     file_id : 1,
      props : this.props
     };
     this.loadFile = this.loadFile.bind(this);
@@ -53,11 +55,25 @@ class Clusterizer extends Component {
 
 
     render(){
+      const { classes } = this.props;
       if (this.state.data == null){
         return ( <Cylon />)
       }
       else {
-        return (<SimpleMap data={this.state.data} lat_col={this.state.lat_col} lon_col={this.state.lon_col} /> )
+        return (
+          <div className={classes.root}>
+      
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+              <Paper className={classes.paper}><SimpleMap data={this.state.data} lat_col={this.state.lat_col} lon_col={this.state.lon_col} /> </Paper>
+              </Grid>
+              <Grid item xs={6}>
+              <Paper className={classes.paper}><DataTable data={this.state.data} header={this.state.cols}/></Paper>
+              </Grid>
+            </Grid>
+        
+          </div>
+        );
       }
     }
 }
