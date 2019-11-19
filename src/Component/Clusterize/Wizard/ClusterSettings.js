15259be18413cb7818a7e0318196b6e08530f132
marcia-t <tejedamarcia@gmail.com>
2019-11-19 16:48:14 -0300

    WIP: wizard

--- src/Component/Clusterize/Wizard/ClusterSettings.js
+++ src/Component/Clusterize/Wizard/ClusterSettings.js
@@ -8,8 +8,9 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-
-
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 
 const Stats = ({
     currentStep,
@@ -43,13 +44,35 @@ class ClusterSettings extends Component {
     constructor(props) {
       super(props);
       this.state = {};
+      if (this.props.file){
+        this.state = {
+          doc_id : this.props.file
+         };
+      }
+      this.loadFile = this.loadFile.bind(this);
     }
   
 
+    async componentDidMount() {
+      this.loadFile();
+      }
+
+
+    async loadFile()
+    {
+        let doc_id = this.state.doc_id
+        const promise = await axios.get("http://localhost:8000/geo_file/"+doc_id)
+        const status = promise.status;
+        if(status===200)
+        {
+            const data = promise.data;
+            this.setState({data:data.rows, lat_col: data.lat_col, lon_col: data.lon_col, cols: data.cols}); 
+        }
+    }
   
     handleRadioChange = event => {
       this.setState({algorithm : event.target.value})
-      this.props.update(event.target.name, event.target.value);
+      //this.props.update(event.target.name, event.target.value);
     }
 
     preview = async () => {
@@ -65,8 +88,8 @@ class ClusterSettings extends Component {
            <label>Select the cluster algorithm </label>
            
             <RadioGroup aria-label="gender" name="algorithm" value={this.state.algorithm} onChange={this.handleRadioChange}>
-                <FormControlLabel value="meanshift" control={<Radio color="primary"/>} label="Mean Shift" />}
-                <FormControlLabel value="kmeans" control={<Radio color="primary"/>} label="K Means" />}
+                <FormControlLabel value="meanshift" control={<Radio color="primary"/>} label="Mean Shift" />
+                <FormControlLabel value="kmeans" control={<Radio color="primary"/>} label="K Means" />
             </RadioGroup>
   
            <hr />
@@ -77,6 +100,23 @@ class ClusterSettings extends Component {
                       onChange={this.handleInputChange} />
             </div>
            }
+           {this.state.data &&
+           <div>
+             <div>
+             <InputLabel id="demo-simple-select-filled-label">Column X</InputLabel>
+              <Select name="col_x" onChange={this.onChange}>
+                    {this.state.cols.map((col) => <MenuItem key={col} value={col}>{col}</MenuItem>)}
+              </Select>
+            </div>
+            <div>
+            <InputLabel id="demo-simple-select-filled-label">Column Y</InputLabel>
+              <Select name="col_y" onChange={this.onChange}>
+                    {this.state.cols.map((col) => <MenuItem key={col} value={col}>{col}</MenuItem>)}
+              </Select>
+            </div>
+            </div>
+           }
+
            
             <Stats step={2}  {...this.props} />
         </div>
